eff80079ce27fe8a1a0cf19daea2f61ce9028ecf
Stephany_moreno <stephanymoreno1826@gmail.com>
2025-11-06 14:06:14 -0500

    el programa evalua los numeros hasta 10 y evalua si el numero es par o no

---
+++ js/ejercicio_1.js
@@ -0,0 +1,10 @@
+//inicializar variable
+let numero = 0;
+
+//ciclo para evaluar números del 1 al 10 y comprobar si son pares 
+for (let numero = 1; numero <= 10; numero++) {
+    if (numero % 2 === 0) {
+        console.log("El número " + numero + " es par");
+    }
+}
+
